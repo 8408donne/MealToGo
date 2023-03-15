a67926b397403f6e134e9ada00e25fae4011599f
Daniel Donnelly <dcdonnelly@hotmail.co.uk>
2023-03-15 16:12:50 +0000

    rebuid

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,7 +1,7 @@
-import { mocks, mockImages } from "./mock";
+import { mocks } from "./mock";
 import camelize from "camelize";
 
-export const restaurantsRequest = (location) => {
+export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
   return new Promise((resolve, reject) => {
     const mock = mocks[location];
     if (!mock) {
@@ -10,20 +10,10 @@ export const restaurantsRequest = (location) => {
     resolve(mock);
   });
 };
-
-export const restaurantsTransform = ({ results = [] }) => {
-  const mappedResults = results.map((restaurant) => {
-    restaurant.photos = restaurant.photos.map((p) => {
-      return mockImages[Math.ceil(Math.random() * (mockImages.length - 1))];
-    });
-
-    return {
-      ...restaurant,
-      address: restaurant.vicinity,
-      isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
-      isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
-    };
+restaurantsRequest()
+  .then((result) => {
+    console.log(camelize(result));
+  })
+  .catch((err) => {
+    console.log("error");
   });
-
-  return camelize(mappedResults);
-};
