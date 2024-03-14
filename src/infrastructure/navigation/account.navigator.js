00c360f814525ac90dbf8284e681a77b6296a6ff
Daniel Donnelly <73676951+8408donne@users.noreply.github.com>
2024-03-14 00:14:07 +0000

    Login and Register

--- src/infrastructure/navigation/account.navigator.js
+++ src/infrastructure/navigation/account.navigator.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { createStackNavigator } from "@react-navigation/stack";
 
-import { MainScreen } from "../../features/account/screens/main.screen";
+import { AccountScreen } from "../../features/account/screens/account.screen";
 import { LoginScreen } from "../../features/account/screens/login.screen";
 import { RegisterScreen } from "../../features/account/screens/register.screen";
 
@@ -10,8 +10,8 @@ const Stack = createStackNavigator();
 
 
 export const AccountNavigator = () => (
-    <Stack.Navigator headerMode="none">
-    <Stack.Screen name="Main" component={MainScreen} />
+    <Stack.Navigator options={{ headerShown: false }}>
+    <Stack.Screen name="Main" component={AccountScreen} />
     <Stack.Screen name="Login" component={LoginScreen} />
     <Stack.Screen name="Register" component={RegisterScreen} />
     </Stack.Navigator>
--- src/services/authentication/authentication.context.js
+++ src/services/authentication/authentication.context.js
@@ -1,41 +1,80 @@
-import React, { createContext, useState } from "react";
-import { initializeApp } from "firebase/app";
+import React, { useState, createContext, useRef } from "react";
+import {
+  signOut,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  getAuth,
+} from "firebase/auth";
 
 import { loginRequest } from "./authentication.service";
 
 export const AuthenticationContext = createContext();
 
 export const AuthenticationContextProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
+  const auth = useRef(getAuth()).current;
+
+  onAuthStateChanged(auth, (usr) => {
+    if (usr) {
+      setUser(usr);
+      setIsLoading(false);
+    } else {
+      setIsLoading(false);
+    }
+  });
 
   const onLogin = (email, password) => {
     setIsLoading(true);
-    setError(null);
-    const app = initializeApp(firebaseConfig);
-    const auth = getAuth(app);
-    signInWithEmailAndPassword(auth, email, password)
+    loginRequest(auth, email, password)
       .then((u) => {
         setUser(u);
         setIsLoading(false);
       })
       .catch((e) => {
-        setError(e);
         setIsLoading(false);
+        setError(e.toString());
       });
+  };
+
+  const onRegister = (email, password, repeatedPassword) => {
+    setIsLoading(true);
+    if (password !== repeatedPassword) {
+      setError("Error: Passwords do not match");
+      return;
+    }
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((u) => {
+        setUser(u);
+        setIsLoading(false);
+      })
+      .catch((e) => {
+        setIsLoading(false);
+        setError(e.toString());
+      });
+  };
+
+  const onLogout = () => {
+    signOut(auth).then(() => {
+      setUser(null);
+      setError(null);
+    });
   };
 
   return (
     <AuthenticationContext.Provider
       value={{
+        isAuthenticated: !!user,
         user,
         isLoading,
         error,
         onLogin,
+        onRegister,
+        onLogout,
       }}
     >
       {children}
     </AuthenticationContext.Provider>
   );
-}
+};
